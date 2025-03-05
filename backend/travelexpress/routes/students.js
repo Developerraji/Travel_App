66d3174cb6ac652300e778c8114138f9b27c7cbb
Developerraji <152413470+Developerraji@users.noreply.github.com>
2025-03-05 15:26:17 +0530

    Update students.js

--- backend/travelexpress/routes/students.js
+++ backend/travelexpress/routes/students.js
@@ -16,38 +16,5 @@ const { response } = require('../app')
     .catch(err=>res.send(err))
 }
 ) 
- //localhost:3000/students/read/Adam
- /*router.get('/read/:firstName',(req,res)=>{
-    studentModel.find({firstName:req.params.firstName})
-    .then(response=>res.send(response))
-    .catch(err=>res.send(err))
-})
-//localhost:3000/student/get/?id=
-/*router.get('/get',(req,res)=>{
-    studentModel.findById(req.query.id)
-    .then(response=>res.send(response))
-    .catch(err=>res.send(err))
-})
-//localhost:3000/student/update/?id=
-router.put('/update',(req,res)=>{
-    const idQuery=req.query.id
-    studentModel.findByIdAndUpdate(idQuery,{age:req.body.age})
-    .then(response=>res.send(response))
-    .catch(err=>res.send(err))
-})
-//localhost:3000/student/update/Adam
-router.put('/update/:name',(req,res)=>{
-    nameParams=req.params.firstName
-    studentModel.findOneAndUpdate(nameParams,{age:req.body.age})
-    .then(response=>res.send(response))
-    .catch(err=>res.send(err))
 
-})
-//localhost:3000/student/delete/?id=
-router.delete('/delete',(req,res)=>{
-    const idQuery=req.query.id
-    studentModel.id=findByIdAndDelete(idQuery)
-    .then(response=>res.send(response))
-    .catch(err=>res.send(err))
-})  */
 module.exports=router
