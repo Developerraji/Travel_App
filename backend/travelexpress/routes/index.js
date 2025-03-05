9178a682ba41f6b5a28ef03489e24f902cfe3bbd
Developerraji <152413470+Developerraji@users.noreply.github.com>
2025-03-05 15:24:52 +0530

    Update index.js

--- backend/travelexpress/routes/index.js
+++ backend/travelexpress/routes/index.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
-/*GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
-/*app.get("http://localhost:3000/api/destinations", (req, res) => {
-  res.json([
-      { id: 1, name: "Santorini, Greece" },
-      { id: 2, name: "Kyoto, Japan" },
-      { id: 3, name: "Maldives" },
-      { id: 4, name: "Swiss Alps, Switzerland" }
-  ]);
-});*/
 
